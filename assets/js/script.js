b116a5a3f337acd82883d832258da266eea438f4
Bryan Rivera Gonzalez <rivera.bryan1406@gmail.com>
2020-07-04 22:19:04 -0400

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -164,7 +164,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -204,15 +204,21 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event){
     console.log("activate", this);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event) {
     console.log("activate", this);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag")
   },
   over: function(event) {
     console.log("over", event.target);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
     console.log("out", event.target);
+    $(event.target).removeClass("dropover-active");
   },
   update: function(event) {
     //array to store the task data in
@@ -259,9 +265,11 @@ $("#trash").droppable({
   },
   over: function(event, ui) {
     console.log("over");
+    $(".bottom-trash").addClass("bottom-trash-active");
   },
   out: function(event, ui) {
     console.log("out");
+    $(".bottom-trash").removeClass("bottom-trash-active");
   }
 });
 
@@ -287,3 +295,9 @@ else if (Math.abs(moment().diff(time, "days")) <= 2) {
   $(taskEl).addClass("list-group-item-warning");
 }
 };
+
+setInterval(function () {
+  $(".card .list-group-item").each(function (el) {
+    auditTask(el);
+  });
+}, (1000 * 60) * 30);
